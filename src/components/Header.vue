1e41c03cdff7457512a9b06ba6f3a61b63a6f5ca
LeezyLazyCrazy <leeg1dance@gmail.com>
2022-01-25 19:42:29 +0900

    logo changed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl mb:text-5xl font-bold mb-3"> 
-     <img src="../assets/logo.png" alt="">
+    <div class="text-3xl mb:text-5xl font-bold mb-3 inline-block"> 
+     <img src="../assets/logo.png" alt="" class="w-16 h-16 inline-block white">
       Covid19 Tracker</div>
     <p>
       API by
